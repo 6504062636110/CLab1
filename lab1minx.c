f6db8c498bfe65ff7b7b95c288307b5e17dc6d13
NattanichaOnsuwan <153050949+6504062636110@users.noreply.github.com>
2024-11-28 18:03:54 +0700

    version2 n1>n2=?

--- lab1minx.c
+++ lab1minx.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	int num1=10 ,num2=20;
-	
-	if(num1>num2){
-		printf("num1 is greater than num2");
-	}
-	if(num2>num1){
-		printf("num2 is greater than num1");
+	int a;
+	int b;
+	scanf("n1=%d \n",&a);
+	scanf("n2=%d",&b);
+	if(a>b){
+		printf("n1>n2=%d \n", a-b);
+	}else{
+		printf("n2>n1=%d \n", b-a);
 	}
 	
 	return 0;
